c8047bb14aa715f1507e581138adc9c6fea9e603
Francesco <francesco.ferruccio88@gmail.com>
2020-04-16 15:55:56 +0200

    modificata funzione "svuotamento" input

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -4,115 +4,120 @@
 // -Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
 // - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-// var stud = {
-//   'nome': "Mario",
-//   'cognome': "Rossi",
-//   'eta': 18
-// }
-//
-// for (var key in stud) {
-//   console.log(key + ": " + stud[key]);
-// }
+$(document).ready(function()
+{
 
-// VARIABILI GLOBALI
-var indice;
+  // var stud = {
+  //   'nome': "Mario",
+  //   'cognome': "Rossi",
+  //   'eta': 18
+  // }
+  //
+  // for (var key in stud) {
+  //   console.log(key + ": " + stud[key]);
+  // }
 
-// inizializzazione handlebars
-var source = $("#studente-template").html();
-var template = Handlebars.compile(source);
+  // VARIABILI GLOBALI
+  var indice;
 
-// creo un array di oggetti "studente"
-var listaStudenti = [
-  {
-    'nome': "Mario",
-    'cognome': "Rossi",
-    'eta': 18
-  },
-  {
-    'nome': "John",
-    'cognome': "Doe",
-    'eta': 19
-  },
-  {
-    'nome': "Aldo",
-    'cognome': "Baglio",
-    'eta': 22
-  },
-  {
-    'nome': "Giuseppe",
-    'cognome': "Verdi",
-    'eta': 31
-  }
-];
+  // inizializzazione handlebars
+  var source = $("#studente-template").html();
+  var template = Handlebars.compile(source);
 
-// stampo in console la lista iniziale
-console.log("LISTA INIZIALE");
+  // creo un array di oggetti "studente"
+  var listaStudenti = [
+    {
+      'nome': "Mario",
+      'cognome': "Rossi",
+      'eta': 18
+    },
+    {
+      'nome': "John",
+      'cognome': "Doe",
+      'eta': 19
+    },
+    {
+      'nome': "Aldo",
+      'cognome': "Baglio",
+      'eta': 22
+    },
+    {
+      'nome': "Giuseppe",
+      'cognome': "Verdi",
+      'eta': 31
+    }
+  ];
 
-for (var i = 0; i < listaStudenti.length; i++) {
-  var studente = listaStudenti[i];
-  for (var key in studente) {
-    console.log(key + ": " + studente[key]);
-  }
-}
+  // stampo in console la lista iniziale
+  console.log("LISTA INIZIALE");
 
-// Stampo in pagina
-
-for (var i = 0; i < listaStudenti.length; i++) {
-  var studente = listaStudenti[i];
-  var context = {number: (i+1), name: studente.nome, surname: studente.cognome, age: studente.eta };
-  var html = template(context);
-
-  $(".container").append(html);
-}
-
-indice = i+1;
-
-// inserimento dinamico nuovi studenti al click sul bottone
-$("button").click(insertStudent);
-
-// inserimento dinamico nuovi studenti premendo invio quando sono in uno dei campi di input
-$("input").focusin().keyup(
-  function (event) {
-    if(event.which == 13) {
-      insertStudent();
+  for (var i = 0; i < listaStudenti.length; i++) {
+    var studente = listaStudenti[i];
+    for (var key in studente) {
+      console.log(key + ": " + studente[key]);
     }
   }
-)
 
+  // Stampo in pagina
 
-// DICHIARAZIONE FUNZIONI
-
-// funzione che inserisce il nuovo studente nell'array e lo stampa in pagina
-function insertStudent() {
-  var inputNome = $("input[name='nome']").val();
-  var inputCognome = $("input[name='cognome']").val();
-  var inputEta = $("input[name='eta']").val();
-
-  // svuoto i campi di input
-  $("input[name='nome']").val("");
-  $("input[name='cognome']").val("");
-  $("input[name='eta']").val("");
-
-  // controllo che i dati inseriti siano corretti
-  if (inputNome != "" && inputCognome != "" && inputEta != "" && !isNaN(inputEta)) {
-    // creo un nuovo oggetto "studente" con i dati inseriti dall'utente
-    var inputObj = {
-      'nome': inputNome,
-      'cognome': inputCognome,
-      'eta': parseInt(inputEta)
-    }
-
-    listaStudenti.push(inputObj);
-
-    var context = {number: indice, name: inputObj.nome, surname: inputObj.cognome, age: inputObj.eta };
+  for (var i = 0; i < listaStudenti.length; i++) {
+    var studente = listaStudenti[i];
+    var context = {number: (i+1), name: studente.nome, surname: studente.cognome, age: studente.eta };
     var html = template(context);
 
     $(".container").append(html);
-    indice++;
-
-    console.log("Array aggiornato", listaStudenti);
-  } else {
-    alert("Controlla i dati inseriti");
   }
 
-}
+  indice = i+1;
+
+  // inserimento dinamico nuovi studenti al click sul bottone
+  $("button").click(insertStudent);
+
+  // inserimento dinamico nuovi studenti premendo invio quando sono in uno dei campi di input
+  $("input").focusin().keyup(
+    function (event) {
+      if(event.which == 13) {
+        insertStudent();
+      }
+    }
+  )
+
+
+  // DICHIARAZIONE FUNZIONI
+
+  // funzione che inserisce il nuovo studente nell'array e lo stampa in pagina
+  function insertStudent() {
+    var inputNome = $("input[name='nome']").val();
+    var inputCognome = $("input[name='cognome']").val();
+    var inputEta = $("input[name='eta']").val();
+
+    // controllo che i dati inseriti siano corretti
+    if (inputNome != "" && inputCognome != "" && inputEta != "" && !isNaN(inputEta)) {
+      // creo un nuovo oggetto "studente" con i dati inseriti dall'utente
+      var inputObj = {
+        'nome': inputNome,
+        'cognome': inputCognome,
+        'eta': parseInt(inputEta)
+      }
+
+      listaStudenti.push(inputObj);
+
+      var context = {number: indice, name: inputObj.nome, surname: inputObj.cognome, age: inputObj.eta };
+      var html = template(context);
+
+      $(".container").append(html);
+      indice++;
+
+      // svuoto i campi di input
+      $("input[name='nome']").val("");
+      $("input[name='cognome']").val("");
+      $("input[name='eta']").val("");
+
+      console.log("Array aggiornato", listaStudenti);
+    } else {
+      alert("Controlla i dati inseriti");
+    }
+
+  }
+
+});
--- js/main.js
+++ js/main.js
@@ -3,76 +3,78 @@
 // - Creare un array di oggetti di studenti.
 // -Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
 // - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+$(document).ready(function()
+{
+  // var stud = {
+  //   'nome': "Mario",
+  //   'cognome': "Rossi",
+  //   'eta': 18
+  // }
+  //
+  // for (var key in stud) {
+  //   console.log(key + ": " + stud[key]);
+  // }
 
-// var stud = {
-//   'nome': "Mario",
-//   'cognome': "Rossi",
-//   'eta': 18
-// }
-//
-// for (var key in stud) {
-//   console.log(key + ": " + stud[key]);
-// }
+  var listaStudenti = [
+    {
+      'nome': "Mario",
+      'cognome': "Rossi",
+      'eta': 18
+    },
+    {
+      'nome': "John",
+      'cognome': "Doe",
+      'eta': 19
+    },
+    {
+      'nome': "Aldo",
+      'cognome': "Baglio",
+      'eta': 22
+    },
+    {
+      'nome': "Giuseppe",
+      'cognome': "Verdi",
+      'eta': 31
+    }
+  ]
 
-var listaStudenti = [
-  {
-    'nome': "Mario",
-    'cognome': "Rossi",
-    'eta': 18
-  },
-  {
-    'nome': "John",
-    'cognome': "Doe",
-    'eta': 19
-  },
-  {
-    'nome': "Aldo",
-    'cognome': "Baglio",
-    'eta': 22
-  },
-  {
-    'nome': "Giuseppe",
-    'cognome': "Verdi",
-    'eta': 31
+  console.log("LISTA INIZIALE");
+
+  for (var i = 0; i < listaStudenti.length; i++) {
+    var studente = listaStudenti[i];
+    for (var key in studente) {
+      console.log(key + ": " + studente[key]);
+    }
   }
-]
 
-console.log("LISTA INIZIALE");
+  var etaValida = false;
 
-for (var i = 0; i < listaStudenti.length; i++) {
-  var studente = listaStudenti[i];
-  for (var key in studente) {
-    console.log(key + ": " + studente[key]);
+  var inputNome = prompt("Inserisci nome");
+  var inputCognome = prompt("Inserisci cognome");
+  var inputEta;
+  do {
+    inputEta = prompt("Inserisci eta");
+    if (!isNaN(inputEta) && inputEta != "") {
+      etaValida = true;
+    } else {
+      alert("Inserisci un'età valida!");
+    }
+  } while (etaValida == false);
+
+  var inputObj = {
+    'nome': inputNome,
+    'cognome': inputCognome,
+    'eta': inputEta
   }
-}
 
-var etaValida = false;
+  listaStudenti.push(inputObj);
 
-var inputNome = prompt("Inserisci nome");
-var inputCognome = prompt("Inserisci cognome");
-var inputEta;
-do {
-  inputEta = prompt("Inserisci eta");
-  if (!isNaN(inputEta) && inputEta != "") {
-    etaValida = true;
-  } else {
-    alert("Inserisci un'età valida!");
+  console.log("LISTA DOPO INSERIMENTO UTENTE");
+
+  for (var i = 0; i < listaStudenti.length; i++) {
+    var studente = listaStudenti[i];
+    for (var key in studente) {
+      console.log(key + ": " + studente[key]);
+    }
   }
-} while (etaValida == false);
-
-var inputObj = {
-  'nome': inputNome,
-  'cognome': inputCognome,
-  'eta': inputEta
-}
-
-listaStudenti.push(inputObj);
-
-console.log("LISTA DOPO INSERIMENTO UTENTE");
-
-for (var i = 0; i < listaStudenti.length; i++) {
-  var studente = listaStudenti[i];
-  for (var key in studente) {
-    console.log(key + ": " + studente[key]);
-  }
-}
+});
